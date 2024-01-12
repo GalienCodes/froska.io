752308a7a5f7f6f146be2c9522a9529cca9d4254
Muhindo-Galien <muhindogalien512@gmail.com>
2024-01-12 22:52:55 +0300

    added mission section

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -10,26 +10,21 @@ interface Mission {
 
 const Mission = () => {
     return (
-        <div className='max-w-6xl mx-auto text-gray-50 pt-8 pb-10 sm:pb-20 '>
-            <h2 className='font-bold text-3xl lg:text-4xl pb-2 top-0  text-center capitalize'>
-                FROSKA's Mission
+        <div className='max-w-5xl mx-auto  pt-20 pb-10 sm:pb-20 '>
+            <h2 className='font-GilroyBold text-xl dark:text-[#FFFFFF] text-[#1F1E1E]'>
+                Our Mission
             </h2>
-            <h4 className='text-base text-center sm:text-lg font-bold sm:font-bold  '>Safeguarding and Incentivizing</h4>
-            <div className='mx-4 sm:mx-6 lg:mx-0 grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6  justify-between  items-start'>
+
+            <div className=' pt-2 grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-12  justify-between  items-start'>
                 {froskaMission.map((item: Mission) => {
                     const { id, title, description } = item;
                     return (
-                        <div className='rounded p-4' key={id}>
-                            <div className=' flex items-center'>
-                                <h1 className='bg-[#5590ff] text-gray-50 w-10 h-10 text-center p-2 rounded-full mb-2  text-xl font-bold shadow-lg'>
-                                    {id}
-                                </h1>
-                                <span className='ml-4'>
-                                    {title}
-                                </span>
-                            </div>
+                        <div className='font-GilroyRegular' key={id}>
+                            <h1 className='font-GilroyHeavyItalic dark:text-[#42454C] text-[#E8E8E8] text-6xl'>
+                                {id}
+                            </h1>
 
-                            <p className='text-sm sm:text-base'>
+                            <p className='mt-[-26px] dark:text-[#FFFFFF] text-[#242529] text-xs'>
                                 {description}
                             </p>
                         </div>)
