ea39263ae4f0bf018daa0e68661533b5058e5be5
Muhindo-Galien <muhindogalien512@gmail.com>
2024-01-12 03:57:58 +0300

    updated set_up

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,11 @@ import Distribution from '@/components/Distribution'
 import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import Mission from '@/components/Mission'
-// import NavBar from '@/components/NavBar'
 import Symbol from '@/components/Symbol'
 
 export default function Home() {
   return (
     <div className='globalFont'>
-      {/* <NavBar/> */}
       <Hero/>
       <Symbol/>
       <Mission/>
