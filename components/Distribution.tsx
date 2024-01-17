716c2c797f373d23306f8005cc622ac1eca2a7e8
Muhindo-Galien <muhindogalien512@gmail.com>
2024-01-17 18:07:30 +0300

    desctribution updated

--- components/Distribution.tsx
+++ components/Distribution.tsx
@@ -13,8 +13,8 @@ const Distribution = () => {
                 <p className='font-GilroyRegular text-xs pt-2.5 pb-5'>Total Token Supply: <span className='font-GilroyBold '>100 Million Tokens</span></p>
                 <div className='text-[#F3F4F5]  pt-7 md:pt-2 grid justify-between  items-center'>
                     <div className='text-left w-full sm:w-3/5'>
-                        {distribution.map((item,index) => {
-                            const {name, mainDescription, description, points, subPoints } = item
+                        {distribution.map((item, index) => {
+                            const { id, name, mainDescription, description, points, subPoints, details } = item
                             return (
                                 <div>
                                     <div className='flex items-start gap-5' key={index}>
@@ -22,12 +22,20 @@ const Distribution = () => {
                                         <div>
                                             <h2 className={`font-GilroyBold text-xs ${index === distribution.length - 1 ? '' : 'pb-5'}`}>{name}</h2>
                                             <p className='font-GilroyRegular text-xs '>{mainDescription}</p>
+                                            {details?.map((item,index) => {
+                                                return (
+                                                    <div className={` ${index === details.length - 1 ? '' : 'py-2'}`} key={index}>
+                                                        <h2 className={`font-GilroyBold text-xs pb-5'}`}>{item.title}</h2>
+                                                        <p className='font-GilroyRegular text-xs '>{item.desc}</p>
+                                                    </div>
+                                                )
+                                            })}
                                             <p className='font-GilroyRegular text-xs mt-4'>{description && description}</p>
                                             <ul className='font-GilroyRegular text-xs ml-4 md:ml-6 mt-4'>
                                                 {points?.map((item, index) => (
                                                     <li className="list-disc" key={index}>{item}</li>
                                                 ))}
-                                                <ul className={`font-GilroyRegular text-xs ml-6 ${ subPoints.length==0 ? '' : 'mb-8'}`}>
+                                                <ul className={`font-GilroyRegular text-xs ml-6 ${subPoints.length == 0 ? '' : 'mb-8'}`}>
                                                     {subPoints?.map((item, index) => (
                                                         <li className="list-disc" key={index}>{item}</li>
                                                     ))}
--- mission.ts
+++ mission.ts
@@ -28,19 +28,15 @@ export const tokenomicItems = [
     },
     {
         id: 2,
-        name: "Marketing and BD"
+        name: "Airdrops and Rewards"
     },
     {
         id: 3,
-        name: "Metis Community Allocation"
+        name: "Marketing and Development "
     },
     {
         id: 5,
-        name: "Community Future rewards program"
-    },
-    {
-        id: 6,
-        name: "Early Contributors, Ecosystem builders, Metis KOLsAdvisors, Froska Ambassadors"
+        name: "Early contributors, Ecosystem builders, Metis KOLs, Advisors and FROSKA Ambassadors"
     },
 ]
 
@@ -58,33 +54,22 @@ export const distribution = [
         name: "Airdrop and Rewards: 30% (30 Million Tokens)",
         mainDescription: "FROSKA aims to be a cornerstone of the Web3 community. Half of the total token supply, 30 million tokens, will be airdropped to the broader Metis Web3 community, fostering engagement and participation across the ecosystem",
         description:"",
+        details:[
+            {
+                title:"Metis Community Allocation: 10% (10 Million Tokens)",
+                desc:"Starting from within, 10 million tokens will be airdropped to Metis community contributors, native developers, and dedicated members, acknowledging their vital role in the ecosystem's growth."
+            },
+            {
+                title:"Community Determined Future Rewards: 20% (20 Million Tokens)",
+                desc:"The remaining 20 million tokens from the Airdrop pool will serve as rewards for Metis users, Froska holders and contributors, incentivizing ongoing contributions and ensuring sustained growth"
+            }
+        ],
         points: [],
         subPoints: []
     },
+
     {
         id: 3,
-        name: "1. Metis Community Allocation: 10% (10 Million Tokens)",
-        mainDescription: "Starting from within, 10 million tokens will be airdropped to Metis community contributors, native developers, and dedicated members, acknowledging their vital role in the ecosystem's growth.",
-        description:"",
-        points: ["MIP2/ MIP3 CEG voters", "Nuvo Contributors"],
-        subPoints: [
-            'Nuvo Participation Badge Holders',
-            'Metis advocates',
-            'Nuvo Festive Spirit Holders',
-            'Nuvo AMA badge owners'
-        ]
-
-    },
-    {
-        id: 4,
-        name: "Community Determined Future Rewards: 20% (20 Million Tokens)",
-        mainDescription: "The remaining 20 million tokens from the Airdrop pool will serve as rewards for Metis users, Froska holders and contributors, incentivizing ongoing contributions and ensuring sustained growth",
-        description:"",
-        points: [],
-        subPoints: []
-    },
-    {
-        id: 5,
         name: "Marketing and Development: 10% (10 Million Tokens)",
         mainDescription: "To secure FROSKA's longevity and advancement, 10 million tokens will be earmarked. This reserve will support crucial aspects such as marketing initiatives, future development endeavors, strategic partnerships, and grants aimed at fostering widespread adoption.",
         description:"Early Contributors, Builders and KOLs: 20% (20 Million Tokens)The vibrant Froska team, Community Verified Project Builders(CVPs), Mouthpieces of our ecosystem (KOLs) and members from our very own OG Metis community, is dedicated to maintaining a safe and welcoming ecosystem.",
